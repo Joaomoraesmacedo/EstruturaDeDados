2100002ea49288fe58bb557e71ad49cec5589bf5
Joaomoraesmacedo <ra139392@uem.br>
2024-12-23 19:54:32 -0300

    Add files via upload

--- TrabalhoPilha.py
+++ TrabalhoPilha.py
@@ -1,6 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass 
+class pilha:
+    elementos: list
+    topo: int
+
+
 #InicializaPilha(P)
 #PilhaVazia(P)
 #PilhaCheia(P)
 #Empilha(P,x)
 #Desempilha(P,x)
 #ElementoDoTopo(P)
+
+def InicializaPilha():
+    return
