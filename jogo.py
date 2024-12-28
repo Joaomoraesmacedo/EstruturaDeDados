e6d3f13e54597d8a4df3b211552fcd0c091a857b
Joaomoraesmacedo <ra139392@uem.br>
2024-12-28 09:48:35 -0300

    Add files via upload

--- jogo.py
+++ jogo.py
@@ -1,8 +1,14 @@
 from pilhas import Pilha
 from random import randint
 
-def add_pilhas(n:int, lista_total: list[Pilha]):
-    "Fazer explicação e testes - João"
+def add_pilhas(n:int, lista_total: list[Pilha]) -> list[Pilha]:
+    '''
+    Adiciona as pilhas necessárias para o jogo, que estão na *lista_total*, em outra lista, contendo a quantidade de pilhas escolhidas pelo usuário *n*,
+    com mais 2 pilhas vazias para a manipulação dos números.
+
+    >>> add_pilhas(4, [p1, p2, p3, p4, p5, p6, p7, p8, p9] )
+    [p1, p2, p3, p4, p5, p6]
+    '''
     lista_util: list = []
     for i in range(n + 2):
         lista_util.append(lista_total[i])
@@ -10,16 +16,45 @@ def add_pilhas(n:int, lista_total: list[Pilha]):
 
 
 def Insere_num(x:int, p:Pilha):
-    "blablabla"
+    '''
+    Insere o número no topo *x*, na pilha *p*, se ela não estiver cheia.
+
+    >>> p = [1, 2, '', '']
+    >>> Insere_num(3, p)
+    >>> p
+    [1, 2, 3, '']
+
+    >>> p = [1, 2, 3, 4]
+    >>> Insere_num(5, p)
+    >>> p
+    [1, 2, 3, 4]
+
+    >>> p = ['', '', '', '']
+    >>> Insere_num(3, p)
+    >>> p
+    [3, '', '', '']
+    '''
     if not p.pilha_cheia():
         p.empilha(x)
 
 def Arruma_pilha(p_inicial: Pilha, p_final:Pilha):
+    '''
+    Transfere todos os elementos da *p_inicial* na *p_final*
+
+    >>> P_final = [1, '', '', '']
+    >>> P_inicial = [4, 3, 2, '']
+    >>> Arruma_pilha(P_inicial, P_final)
+    >>> P_final
+    [1, 2, 3, 4]
+    >>> P_inicial
+    ['', '', '', '']
+    '''
+
     while not p_inicial.pilha_vazia():
                     y = p_inicial.desempilha()
                     p_final.empilha(y)
 
-def Verifica_x(x: int, Lista:list[Pilha]): #Usar pilha aux: João
+def Verifica_x(x: int, Lista:list[Pilha]) -> bool: #Usar pilha aux: João
     contador = 0 #N 
     for pilha in Lista: #N 
         for j in range(pilha.topo+1): #Usar o arruma pilha 
@@ -31,6 +66,16 @@ def Verifica_x(x: int, Lista:list[Pilha]): #Usar pilha aux: João
         return True
 
 def Cria_Pilhas_Aleatórias(n: int, pilha:Pilha, Lista: list[Pilha]):
+    '''
+    Cria uma pilha com números aleatórios que estão entre 1 e *n*
+
+    >>> p = ['', '', '', '']
+    >>> n = 3
+    >>> Lista = [p1, p2, p3, p4, p5, p6, p7, p8, p9]
+    >>> Cria_Pilhas_Aleatórias(n, p, Lista)
+    >>> p
+    [3, 2, 2, 1] 
+    '''
     while not pilha.pilha_cheia():
         x: int = randint(1, n)
         while not Verifica_x(x,Lista) == True:
@@ -39,6 +84,17 @@ def Cria_Pilhas_Aleatórias(n: int, pilha:Pilha, Lista: list[Pilha]):
 
 
 def Troca_Pilha(P_Origem: Pilha, P_Destino: Pilha): 
+    '''
+    Transfere o elemento do topo da *P_Origem* para o topo da *P_Destino*, porém, a *P_Destino* não pode estar cheia,
+    a *P_Origem* não pode estar vazia, e o elemento do topo da *P_Origem* deve ser igual ao elemento do topo da *P_Destino*
+    >>> p = [2, 3, '', '']
+    >>> p1 = [1, 3, '', '']
+    >>> Troca_Pilha(p, p1)
+    >>> p
+    [2, '', '', '']
+    >>> p1
+    [1, 3, 3, '']
+    '''
     if not P_Origem.pilha_vazia():
         if not P_Destino.pilha_cheia():   
             if  P_Destino.pilha_vazia() or P_Origem.elemento_do_topo() == P_Destino.elemento_do_topo():    
@@ -51,8 +107,14 @@ def Troca_Pilha(P_Origem: Pilha, P_Destino: Pilha):
     else:
         print("Troca inválida: A pilha de origem está vazia!")
 
-def Ultimo_elem(P: Pilha):
-    "blabla"
+def Ultimo_elem(P: Pilha) -> int:
+    '''
+    Encontra o último elemento da pilha *P*
+
+    >>> p = [1, 4, 3, 2]
+    >>> Ultimo_elem(p)
+    1
+    '''
     pAux = Pilha()
     Aux = int
     topo = P.topo
@@ -69,8 +131,24 @@ def Ultimo_elem(P: Pilha):
 
     return y
 
-def Verifica_pilha(P_verificar: Pilha):
-    "blabla"
+def Verifica_pilha(P_verificar: Pilha) -> bool:
+    '''
+    Retorna True se *P_verificar* está cheia com todos os elementos iguais ou se a pilha está vazia, 
+    caso contrário retorna False
+
+    >>> p = [3, 3, 3, 3]
+    >>> Verifica_pilha(p)
+    True
+    >>> p1 = ['', '', '', '']
+    >>> Verifica_pilha(p1)
+    True
+    >>> p2 = [3, 3, 3, '']
+    >>> Verifica_pilha(p2)
+    False
+    >>> p3 = [3, 1, 3, 2]
+    >>> Verifica_pilha(p3)
+    False    
+    '''
     if  P_verificar.pilha_vazia(): #Pilha_Vazia -> correto
         return True
     p_Armazenador = Pilha()
@@ -89,12 +167,28 @@ def Verifica_pilha(P_verificar: Pilha):
     return True
 
 def Verifica_todas_pilhas(Lista: list[Pilha]) -> bool:
+    '''
+    Retorna True se todas as pilhas da *Lista* está cheia com todos os elementos iguais ou se a pilha está vazia, 
+    caso contrário retorna False
+    >>> p = [[3, 3, 3, 3], [2, 2, 2, 2], [1, 1, 1, 1], ['', '', '', ''], ['', '', '', '']]
+    >>> Verifica_todas_pilhas(p)
+    True
+    >>> p1 = [[3, 3, 2, 3], [2, 2, 1, 2], [1, 1, 3, 1], ['', '', '', ''], ['', '', '', '']]
+    >>> Verifica_todas_pilhas(p1)
+    False
+    >>> p2 = [[3, 3, 3, 3], [2, 2, 2, 2], [1, 1, 1, ''], [2, '', '', ''], ['', '', '', '']]
+    >>> Verifica_todas_pilhas(p1)
+    False
+    '''
     for pilha in Lista:
         if Verifica_pilha(pilha) == False:  # Usa a função existente
             return False
     return True
 
-"""def imprime_lista(Lista: list[Pilha],n):
+def imprime_lista(Lista: list[Pilha],n):
+    '''
+    Pedir ajuda pro meu amor
+    '''
     num: int = n-1
     aux = Pilha()
     for pilha in Lista:
@@ -104,7 +198,7 @@ def Verifica_todas_pilhas(Lista: list[Pilha]) -> bool:
             aux.empilha(x)
         print(f"P{n-num}")
         num -= 1
-        Arruma_pilha(aux, pilha)"""
+        Arruma_pilha(aux, pilha)
         
 
         
@@ -123,7 +217,7 @@ def main():
     n: int = int(input("Digite o numero de pilhas:  "))
     Lista:list = add_pilhas(n, Lista_de_pilhas_total)
     
-    #Gera todas as pilhas com números 
+    #Gera n pilhas com números aleatórios 
     contador:int = 0
     while contador != n:
         Cria_Pilhas_Aleatórias(n, Lista[contador],Lista)
@@ -137,7 +231,7 @@ def main():
 
     #Verifica a quantidade de trocas necessárias para vencer o jogo 
     cont: int = 0
-    #BLABLABLA
+    #Interface do jogo, interação com o usuário e verificação da conclusão do jogo
     while Verifica_todas_pilhas(Lista) == False:
         p_desempilha = int(input("Digite o número da pilha de origem: "))
         p_empilha = int(input("Digite o número da pilha de destino: "))
@@ -145,7 +239,7 @@ def main():
         cont +=1
         for i, pilha in enumerate(Lista): #printar na vertical
             print(f"Pilha {i + 1}: {pilha.elem}")
-    
+    #Mensagem da conclusão do jogo
     print("Você venceu o jogo após:", cont, "trocas")
 
  
